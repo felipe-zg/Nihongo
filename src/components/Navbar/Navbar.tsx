b55a4426e6c9da62148870d9404f33b2d635c46a
Felipe Zeba <felipe_zeba@outlook.com>
2024-12-27 00:29:19 +0900

    feat: create navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { Box, Button, HamburgerIcon, VStack } from 'native-base';
+import { useScreenWidth } from '../../hooks';
+
+const routes = [
+  { name: 'Home', path: '/' },
+  { name: 'Kanji', path: '/kanji' },
+  { name: 'Words', path: '/words' },
+  { name: 'Periods', path: '/periods' },
+  { name: 'Days of the month', path: '/days-of-the-month' },
+  { name: 'Potential form', path: '/potential-form' },
+  { name: 'Words quizz', path: '/words-quizz' },
+]
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const screenWidth = useScreenWidth();
+
+  const getLinkStyle = (isActive: boolean) => ({
+    color: isActive ? 'white' : 'white',
+    fontWeight: isActive ? 'bold' : 'normal',
+  });
+
+  const Menu = () => (
+    <VStack space={4} justifyContent="center">
+        {routes.map((route) => (
+          <NavLink key={route.path} to={route.path}>
+            {({ isActive }) => (
+              <Button variant="ghost" _text={getLinkStyle(isActive)}>
+                {route.name}
+              </Button>
+            )}
+          </NavLink>
+        ))}
+      </VStack>
+  );
+
+  return screenWidth < 900 ? (
+    <>
+      {isOpen && (
+        <Box bg="darkblue.900" px="4" py="3" width="100%" height="100%">
+          <Menu />
+        </Box>
+      )}
+      {!isOpen && (
+        <Button onPress={() => setIsOpen(true)} variant="solid" colorScheme="light" position="absolute" top="10px" left="10px" zIndex={2}>
+          <HamburgerIcon color="white" />
+        </Button>
+      )}
+    </>
+  ) : (
+    <Box bg="darkblue.900" px="4" py="3" width="15%" height="100%">
+      <Menu />
+    </Box>
+  );
+};
+
+export default Navbar;
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Accordion } from './Accordion/Accordion';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as CheckboxDropdown } from './CheckboxDropdown/CheckboxDropdown';
