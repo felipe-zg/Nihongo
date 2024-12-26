a923654fbb8f346b8ff41cbe26a346fb5afdd813
Felipe Zeba <felipe_zeba@outlook.com>
2024-12-27 00:32:17 +0900

    chore: cleanup

--- src/pages/YDCWords/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as YDCWords4th } from "./4th";
-export { default as YDCWords5th } from "./5th";
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,6 @@
 export { default as Deck } from "./Deck";
-export { default as Kanji } from "./Kanji";
 export { default as KanjiYDC } from "./KanjiYDC";
 export { default as WordsYDC } from "./WordsYDC";
 export { default as WordsYDCQuizz } from "./WordsYDCQuizz";
 export { default as DaysOfTheMonth } from "./DaysOfTheMonth";
 export { default as Periods } from "./Periods";
-export * from "./YDCWords";
