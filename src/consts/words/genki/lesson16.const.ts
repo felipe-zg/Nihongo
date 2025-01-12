fb03a2c8d58e8e1fb821e725e21bf46e1f7b685a
Felipe Zeba <felipe_zeba@outlook.com>
2025-01-12 12:41:12 +0900

    feat: add words for genki lesson 16

---
+++ src/consts/words/genki/lesson16.const.ts
@@ -0,0 +1,346 @@
+export const WORDS_GENKI_LESSON_16: Array<Word> = [
+  {
+    word: '親',
+    kana: 'おや',
+    meaning: ['Parent'],
+    example: '親と話します。',
+    exampleMeaning: 'I will talk to my parent.',
+    type: 'NOUN'
+  },
+  {
+    word: '親戚',
+    kana: 'しんせき',
+    meaning: ['Relative'],
+    example: '親戚に会います。',
+    exampleMeaning: 'I will meet my relatives.',
+    type: 'NOUN'
+  },
+  {
+    word: '砂糖',
+    kana: 'さとう',
+    meaning: ['Sugar'],
+    example: '砂糖を入れます。',
+    exampleMeaning: 'I will put sugar in.',
+    type: 'NOUN'
+  },
+  {
+    word: 'ファイル',
+    kana: 'ファイル',
+    meaning: ['File'],
+    example: 'ファイルを開きます。',
+    exampleMeaning: 'I will open the file.',
+    type: 'NOUN'
+  },
+  {
+    word: '大きさ',
+    kana: 'おおきさ',
+    meaning: ['Size'],
+    example: '大きさを測ります。',
+    exampleMeaning: 'I will measure the size.',
+    type: 'NOUN'
+  },
+  {
+    word: '道',
+    kana: 'みち',
+    meaning: ['Road', 'way', 'direction'],
+    example: '道を渡ります。',
+    exampleMeaning: 'I will cross the road.',
+    type: 'NOUN'
+  },
+  {
+    word: '期末試験',
+    kana: 'きまつしけん',
+    meaning: ['Final exam'],
+    example: '期末試験があります。',
+    exampleMeaning: 'There is a final exam.',
+    type: 'NOUN'
+  },
+  {
+    word: '研究',
+    kana: 'けんきゅう',
+    meaning: ['Research'],
+    example: '研究をします。',
+    exampleMeaning: 'I will do research.',
+    type: 'NOUN'
+  },
+  {
+    word: '奨学金',
+    kana: 'しょうがくきん',
+    meaning: ['Scholarship'],
+    example: '奨学金をもらいます。',
+    exampleMeaning: 'I will receive a scholarship.',
+    type: 'NOUN'
+  },
+  {
+    word: '推薦状',
+    kana: 'すいせんじょう',
+    meaning: ['Letter of recommendation'],
+    example: '推薦状を書きます。',
+    exampleMeaning: 'I will write a letter of recommendation.',
+    type: 'NOUN'
+  },
+  {
+    word: '台風',
+    kana: 'たいふう',
+    meaning: ['Typhoon'],
+    example: '台風が来ます。',
+    exampleMeaning: 'A typhoon is coming.',
+    type: 'NOUN'
+  },
+  {
+    word: '文化',
+    kana: 'ぶんか',
+    meaning: ['Culture'],
+    example: '文化を勉強します。',
+    exampleMeaning: 'I will study culture.',
+    type: 'NOUN'
+  },
+  {
+    word: '返事',
+    kana: 'へんじ',
+    meaning: ['Reply', 'response'],
+    example: '返事をします。',
+    exampleMeaning: 'I will reply.',
+    type: 'NOUN'
+  },
+  {
+    word: '汚い',
+    kana: 'きたない',
+    meaning: ['Dirty'],
+    example: '部屋が汚い。',
+    exampleMeaning: 'The room is dirty.',
+    type: 'い Adjective'
+  },
+  {
+    word: '起こす',
+    kana: 'おこす',
+    meaning: ['To wake (someone) up'],
+    example: '友達を起こします。',
+    exampleMeaning: 'I will wake my friend up.',
+    type: 'う Verb'
+  },
+  {
+    word: '奢る',
+    kana: 'おごる',
+    meaning: ['To treat (someone) to a meal (Person に meal を)'],
+    example: '友達をごします。',
+    exampleMeaning: 'I will treat my friend to a meal.',
+    type: 'う Verb'
+  },
+  {
+    word: '笑う',
+    kana: 'わらう',
+    meaning: ['To laugh'],
+    example: '友達と笑います。',
+    exampleMeaning: 'I will laugh with my friend.',
+    type: 'う Verb'
+  },
+  {
+    word: '落ち込む',
+    kana: 'おちこむ',
+    meaning: [ 'to get depressed', 'To feel down', 'to feel sad'],
+    example: '落ち込んでいる。',
+    exampleMeaning: 'I am feeling down.',
+    type: 'う Verb'
+  },
+  {
+    word: '困る',
+    kana: 'こまる',
+    meaning: ['to have difficulty', 'To be in trouble'],
+    example: '困っています。',
+    exampleMeaning: 'I am in trouble.',
+    type: 'う Verb'
+  },
+  {
+    word: '直す',
+    kana: 'なおす',
+    meaning: ['To fix', 'to repair'],
+    example: '車を直します。',
+    exampleMeaning: 'I will fix the car.',
+    type: 'う Verb'
+  },
+  {
+    word: '見つかる',
+    kana: 'みつかる',
+    meaning: ['To be found'],
+    example: '財布が見つかりました。',
+    exampleMeaning: 'The wallet was found.',
+    type: 'う Verb'
+  },
+  {
+    word: '訳す',
+    kana: 'やくす',
+    meaning: ['To translate (source を target に)'],
+    example: '英語を日本語に訳します。',
+    exampleMeaning: 'I will translate English into Japanese.',
+    type: 'う Verb'
+  },
+  {
+    word: '連れて行く',
+    kana: 'つれていく',
+    meaning: ['To take (someone) to (place) (Person を place に)'],
+    example: '友達を映画に連れて行きます。',
+    exampleMeaning: 'I will take my friend to the movie.', 
+    type: 'う Verb'
+  },
+  {
+    word: '道に迷う',
+    kana: 'みちにまよう',
+    meaning: ['To get lost'],
+    example: '道に迷っています。',
+    exampleMeaning: 'I am lost.',
+    type: 'う Verb'
+  },
+  {
+    word: '迎えに行く',
+    kana: 'むかえにいく',
+    meaning: ['To go to pick up (Place まで/に Person を)'],
+    example: '空港に友達を迎えに行きます。',
+    exampleMeaning: 'I will go to the airport to pick up my friend.',
+    type: 'う Verb'
+  },
+  {
+    word: '集める',
+    kana: 'あつめる',
+    meaning: ['To collect', 'to gather'],
+    example: 'お金を集めます。',
+    exampleMeaning: 'I will collect money.',
+    type: 'る Verb'
+  },
+  {
+    word: '見せる',
+    kana: 'みせる',
+    meaning: ['To show'],
+    example: '写真を見せます。',
+    exampleMeaning: 'I will show a picture.',
+    type: 'る Verb'
+  },
+  {
+    word: '乗り遅れる',
+    kana: 'のりおくれる',
+    meaning: ['To miss (a bus, train, etc.) (vehicle に)'],
+    example: 'バスに乗り遅れました。',
+    exampleMeaning: 'I missed the bus.',
+    type: 'る Verb'
+  },
+  {
+    word: 'アイロンをかける',
+    kana: 'アイロンをかける',
+    meaning: ['To iron (clothes)(〜に)'],
+    example: 'シャツにアイロンをかけます。',
+    exampleMeaning: 'I will iron the shirt.',
+    type: 'る Verb'
+  },
+  {
+    word: '朝寝坊する',
+    kana: 'あさねぼうする',
+    meaning: ['To oversleep'],
+    example: '朝寝坊してしまいました。',
+    exampleMeaning: 'I overslept this morning.',
+    type: 'IRREGULAR'
+  },
+  {
+    word: '案内する',
+    kana: 'あんないする',
+    meaning: ['To guide', 'to show (someone) around (please を'],
+    example: '北海道を案内します。',
+    exampleMeaning: 'I will guide you around Hokkaido.',
+    type: 'IRREGULAR'
+  },
+  {
+    word: '説明する',
+    kana: 'せつめいする',
+    meaning: ['To explain'],
+    example: '説明します。',
+    exampleMeaning: 'I will explain.',
+    type: 'IRREGULAR'
+  },
+  {
+    word: '迎えに来る',
+    kana: 'むかえにくる',
+    meaning: ['To come to pick up (Place まで/に Person を)'],
+    example: '空港に迎えに来ます。',
+    exampleMeaning: 'I will come to pick you up at the airport.',
+    type: 'IRREGULAR'
+  },
+  {
+    word: '今日中に',
+    kana: 'きょうじゅうに',
+    meaning: ['By the end of today'],
+    example: '今日中に終わります。',
+    exampleMeaning: 'It will be finished by the end of today.',
+    type: 'PHRASE'
+  },
+  {
+    word: '授業中に',
+    kana: 'じゅぎょうちゅうに',
+    meaning: ['During class', 'in class'],
+    example: '授業中に寝ないでください。',
+    exampleMeaning: 'Please do not sleep during class.',
+    type: 'PHRASE'
+  },
+  {
+    word: 'この間',
+    kana: 'このあいだ',
+    meaning: ['The other day', 'recently'],
+    example: 'この間、友達に会いました。',
+    exampleMeaning: 'I met my friend the other day.',
+    type: 'PHRASE'
+  },
+  {
+    word: 'これから',
+    kana: 'これから',
+    meaning: ['From now on', 'from this point on'],
+    example: 'これから勉強します。',
+    exampleMeaning: 'I will study from now on.',
+    type: 'PHRASE'
+  },
+  {
+    word: 'このぐらい',
+    kana: 'このぐらい',
+    meaning: ['About this much', 'about this amount'],
+    example: 'このぐらいの大きさです。',
+    exampleMeaning: 'It is about this size.',
+    type: 'PHRASE'
+  },
+  {
+    word: '自分で',
+    kana: 'じぶんで',
+    meaning: ['(Do something) By oneself', 'on one\'s own'],
+    example: '自分で書いてください',
+    exampleMeaning: 'Please write it by yourself.',
+    type: 'PHRASE'
+  },
+  {
+    word: '他の',
+    kana: 'ほかの',
+    meaning: ['Other', 'another'],
+    example: '他の人',
+    exampleMeaning: 'Another person',
+    type: 'PHRASE'
+  },
+  {
+    word: '実は',
+    kana: 'じつは',
+    meaning: ['Actually', 'as a matter of fact'],
+    example: '実は、日本語が話せます。',
+    exampleMeaning: 'Actually, I can speak Japanese.',
+    type: 'PHRASE'
+  },
+  {
+    word: '〜以外',
+    kana: '〜いがい',
+    meaning: ['Except for...', 'other than...'],
+    example: '日本以外の国',
+    exampleMeaning: 'Countries other than Japan',
+    type: 'PHRASE'
+  },
+  {
+    word: '失礼します',
+    kana: 'しつれいします',
+    meaning: ['Excuse me', 'Sorry to interrupt'],
+    example: '失礼しますが、お名前は？',
+    exampleMeaning: 'Excuse me, what is your name?',
+    type: 'PHRASE'
+  },
+]
