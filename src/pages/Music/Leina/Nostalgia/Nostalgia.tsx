d587ecdfc4ddba9f64ccd8bbef66fe018f907ea0
Felipe Zeba <felipe_zeba@outlook.com>
2025-07-08 03:48:56 +0900

    refactor: fontsize and spacings

--- src/pages/Music/Leina/Nostalgia/Nostalgia.tsx
+++ src/pages/Music/Leina/Nostalgia/Nostalgia.tsx
@@ -6,7 +6,7 @@ import { LEINA_NOSTALGIA } from "../../../../consts/music/Leina/nostalgia.const"
 const Nostalgia: React.FC = () => {
 
   return (
-    <Box padding="1cm" backgroundColor={"white"} flex={1} minHeight={"100vh"}>
+    <Box paddingTop={50} paddingLeft={{base: 0, md: 50}} backgroundColor={"white"} flex={1} minHeight={"100vh"}>
       <VStack space={2} p={4}>
         {LEINA_NOSTALGIA.map((line, i) => (
           <LyricLine key={i} rubyText={line} />
--- src/pages/Music/components/RubyWord.tsx
+++ src/pages/Music/components/RubyWord.tsx
@@ -1,14 +1,14 @@
 import { Text, VStack } from "native-base";
 
 export const RubyWord = ({ kanji, furigana }: RubyPart) => (
-  <VStack alignItems="center" mx={0.5}>
+  <VStack alignItems="center">
     <Text
-      fontSize="md"
+      fontSize={{base: "md", md: "xl"}}
       lineHeight="xs"
       opacity={furigana ? 1 : 0} // Hide but keep space
     >
       {furigana || "あ"} {/* Filler kana for consistent height */}
     </Text>
-    <Text fontSize="2xl">{kanji}</Text>
+    <Text fontSize={{base: "2xl", md: "3xl"}}>{kanji}</Text>
   </VStack>
 );
