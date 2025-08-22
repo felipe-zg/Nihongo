e2506ff62edbbd3d44478b343578314c70bb8f2c
Felipe Zeba <felipe_zeba@outlook.com>
2025-08-22 12:56:10 +0900

    feat: increase story font size

--- src/pages/MiniStory/MiniStory.tsx
+++ src/pages/MiniStory/MiniStory.tsx
@@ -38,10 +38,10 @@ const MiniStory: React.FC<MiniStoryProps> = ({ story, selectedStory, onStoryChan
     return (
       <HStack>
         <Box flex={1} alignItems={"center"} justifyContent={"flex-start"} mt={1}>
-          <p style={{marginTop: "0.5rem", marginBottom: "0.5rem", lineHeight: "2rem"}}>{rubypart.character}:</p>
+          <p style={{ fontSize: "1.3rem",  marginTop: "0.5rem", marginBottom: "0.5rem", lineHeight: "2rem"}}>{rubypart.character}:</p>
         </Box>
         <Box flex={11} key={index}>
-          <p style={{marginTop: "0.5rem", marginBottom: "0.5rem", lineHeight: "2rem"}} dangerouslySetInnerHTML={{ __html: formattedDialogue }}  />
+          <p style={{ fontSize: "1.3rem", marginTop: "0.5rem", marginBottom: "0.5rem", lineHeight: "2rem"}} dangerouslySetInnerHTML={{ __html: formattedDialogue }}  />
         </Box>
       </HStack>
     )
