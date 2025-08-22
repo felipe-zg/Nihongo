4d2631610cdef35600de0dd8ad9ada0ea882c2b2
Felipe Zeba <felipe_zeba@outlook.com>
2025-08-22 16:01:31 +0900

    feat: add N2 stories

---
+++ src/consts/MiniStory/N2.const.ts
@@ -0,0 +1,80 @@
+export const MINI_STORY_N2: Record<string, MiniStory> = {
+  "1": {
+    id: "1",
+    rubyStory: [
+      {
+        character: "A",
+        dialogue: "先週行った駅前の<創{そう}作{さく}>料理の店、閉{へい}店{てん}するんだって。",
+      },
+      {
+        character: "B",
+        dialogue: "え、あの店、<食{しょく}物{もつ}>アレルギーに対{たい}応{おう}したメニューもあってよかったのに。",
+      },  
+      {
+        character: "A",
+        dialogue: "ね。駅前は、チェーンのファミレスも多いし、やっぱり、小さい店がそういう大きい店と<勝{しょう}負{ぶ}する>のは難しいのかもね。",
+      },
+    ],
+    rubyWords: [
+      {
+        kanji: "創{そう}作{さく}",
+        english: "creative, create"
+      },
+      {
+        kanji: "食{しょく}物{もつ}",
+        english: "food"
+      },
+      {
+        kanji: "勝{しょう}負{ぶ}[する]",
+        english: "Victory, to compete"
+      },
+    ],
+    translation: `A: I heard that the creative cuisine restaurant by the station that we went to last week is closing.\nB: What? That restaurant was great—it had a menu for people with food allergies.\nA: Yeah. There are lots of franchise family restaurants near the station, and it's probably difficult for a smaller restaurant to compete with such large places.`,
+    audio: "T1.mp3"
+  },
+  "2": {
+    id: "2",
+    rubyStory: [
+      {
+        character: "",
+        dialogue: "日本の米は、品種によって<粒{つぶ}>の大きさ、甘<み>、食{しょっ}感{かん}などが異{こと}なる。300gの少{しょう}量{りょう}<パック>も売っているので、いろいろと試{ため}してみるのもいいだろう。保{ほ}存{ぞん}するときは、温{あたた}かいうちに1<人{にん}前{まえ}>ずつ<ラップ>で包{つつ}むのが大切だ。<冷{さ}めたら>さらに<アルミホイル>で包{つつ}んで冷{れい}凍{とう}庫{こ}に入{い}れれば、おいしいまま冷{れい}凍{とう}できる。",
+      },
+    ],
+    rubyWords: [
+      {
+        kanji: "粒{つぶ}",
+        english: "grain"
+      },
+      {
+        kanji: "〜み",
+        english: "〜ness"
+      },
+      {
+        kanji: "パック［する］",
+        english: "to pack / package"
+      },
+      {
+        kanji: "〜人前",
+        english: "serving / portion"
+      },
+      {
+        kanji: "ラップ［する］",
+        english: "plastic wrap / to wrap"
+      },
+      {
+        kanji: "冷{さ}める",
+        english: "to cool down"
+      },
+      {
+        kanji: "冷{さ}ます",
+        english: "to cool down"
+      },
+      {
+        kanji: "アルミホイル",
+        english: "aluminum foil"
+      },
+    ],
+    translation: `Japanese rice varies in grain size, sweetness, and texture, depending on the variety. Small 300 gram packs are available, so it's a good idea to try different varieties. When storing rice, its important to wrap it in single portions in plastic wrap while it's still warm. After they cool, wrap them in aluminum foil and put them in the freezer to freeze them while they are still full of flavor.`,
+    audio: "T2.mp3"
+  },
+};
--- src/consts/MiniStory/index.ts
+++ src/consts/MiniStory/index.ts
@@ -1,1 +1,2 @@
+export * from './N2.const';
 export * from './N3.const';
