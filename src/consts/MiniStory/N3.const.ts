88ae735eee5d62b72f599375e65ee087bf153529
Felipe Zeba <felipe_zeba@outlook.com>
2025-08-22 21:18:34 +0900

    feat: add ministories N3 pages 16 and 17

--- src/consts/MiniStory/N3.const.ts
+++ src/consts/MiniStory/N3.const.ts
@@ -93,9 +93,201 @@ export const MINI_STORY_N3: Record<string, MiniStory> = {
         english: "amount"
       },
     ],
-    translation: `A: I heard that you should eat fruit for breakfast, but peeling it is a hassle.\nB: Then canned food would be fine, wouldn't it?\nA: But isn't fresh fruit better?`,
+    translation: `Frozen foods these days are very delicious. They are indistinguishable from homemade dishes. Besides, if you divide the tray, you can use only the amount you need, such as for enough for three people or just one person.`,
     audio: "T2.mp3",
     topic: MiniStoryTopics.FOOD,
     page: 14,
+  },
+  "3": {
+    id: "3",
+    rubyStory: [
+      {
+        character: "A",
+        dialogue: "あのう、今日の<日{ひ}替{が}り<定{てい}食{しょく}>って何ですか。",
+      },
+      {
+        character: "B",
+        dialogue: "<本日>は、<エビ><フライ>と<おかず>が1つ、あとはご飯{はん}と<みそ汁{しる}>です。",
+      },
+    ],
+    rubyWords: [
+      {
+        kanji: "日{ひ}替{が}り",
+        english: "daily special"
+      },
+      {
+        kanji: "定{てい}食{しょく}",
+        english: "set meal"
+      },
+      {
+        kanji: "本日",
+        english: "today"
+      },
+      {
+        kanji: "エビ",
+        english: "shrimp"
+      },
+      {
+        kanji: "フライ",
+        english: "fried/fry"
+      },
+      {
+        kanji: "おかず",
+        english: "side dish"
+      },
+      {
+        kanji: "みそ",
+        english: "miso"
+      },
+      {
+        kanji: "汁{しる}",
+        english: "soup"
+      },
+    ],
+    translation: `A:Um, what's in the daily set meal?\nB: Today, we have fried shrimp and one side dish, and also rice and miso soup.`,
+    audio: "T3.mp3",
+    topic: MiniStoryTopics.FOOD,
+    page: 15,
+  },
+  "4": {
+    id: "4",
+    rubyStory: [
+      {
+        character: "A",
+        dialogue: "<お待たせしました>。和{わ}<風{ふう}>ハンバーグでございます。<ソース>をかけて、お<召{め}し上がり>ください。",
+      },
+      {
+        character: "B",
+        dialogue: "あ、どうぞ<お先{さき}に>。<ご遠{えん}慮{りょ}なく>。",
+      },
+      {
+        character: "C",
+        dialogue: "じゃあ、いただきます。",
+      },
+    ],
+    rubyWords: [
+      {
+        kanji: "お待たせしました",
+        english: "Thank you for waiting."
+      },
+      {
+        kanji: " お待ちどおさま",
+        english: "I'm sorry to have kept you waiting."
+      },
+      {
+        kanji: "〜風{ふう}",
+        english: "style"
+      },
+      {
+        kanji: "ソース",
+        english: "sauce"
+      },
+      {
+        kanji: "召{め}し上がる",
+        english: "to eat, drink (honorific)"
+      },
+      {
+        kanji: "（お）先{さき}に",
+        english: "After you"
+      },
+      {
+        kanji: "（ご）遠{えん}慮{りょ}なく",
+        english: "Without hesitation, No need to hesitate"
+      },
+    ],
+    translation: `A: Thank you for your patience. Here is your Japanese-style hamburger steak. Please enjoy it with sauce.\nB: Oh, please go ahead. No need to hesitate.\nC: Then let's eat.`,
+    audio: "T4.mp3",
+    topic: MiniStoryTopics.FOOD,
+    page: 16,
+  },
+  "5": {
+    id: "5",
+    rubyStory: [
+      {
+        character: "",
+        dialogue: "<焼{やき}肉{にく}>の<バイキング>は<セルフサービス>ではなく、テーブルで注文するスタイルが多い。肉{にく}以{い}外{がい}にも、<ライス>、<スープ>、<デザート>などが注文できる。もちろん、<残{のこ}して>はいけない。",
+      },
+    ],
+    rubyWords: [
+      {
+        kanji: "焼{やき}肉{にく}",
+        english: "grilled meat"
+      },
+      {
+        kanji: "バイキング",
+        english: "buffet"
+      },
+      {
+        kanji: "セルフサービス",
+        english: "self-service"
+      },
+      {
+        kanji: "ライス",
+        english: "rice"
+      },
+      {
+        kanji: "スープ",
+        english: "soup"
+      },
+      {
+        kanji: "デザート",
+        english: "dessert"
+      },
+      {
+        kanji: "残{のこ}す",
+        english: "to leave (behind), to waste"
+      },
+    ],
+    translation: `Yakiniku buffet is not self-service, but is often ordered at the table. Besides meat, you can order rice, soup, dessert and more. Of course, you must not leave any leftovers.`,
+    audio: "T5.mp3",
+    topic: MiniStoryTopics.FOOD,
+    page: 16,
+  },
+  "6": {
+    id: "6",
+    rubyStory: [
+      {
+        character: "A",
+        dialogue: " あ、その肉、早く<くっくり返{かえ}して>。。これもこれも。",
+      },
+      {
+        character: "B",
+        dialogue: "うわ、肉が<燃{も}えた>！どうしよう！",
+      },
+      {
+        character: "A",
+        dialogue: "あー、そういうときは、この<氷{こおり}>を網{あみ}に乗{の}せればいいんだ。網{あみ}が汚{よご}れちゃったね。<取{と}り替{か}えて>もらおう。",
+      },
+    ],
+    rubyWords: [
+      {
+        kanji: "ひっくり返{かえ}す",
+        english: "to turn over"
+      },
+      {
+        kanji: "ひっくり返{かえ}る",
+        english: "to be turned over"
+      },
+      {
+        kanji: "燃{も}える",
+        english: "to burn / be burned"
+      },
+      {
+        kanji: "燃{も}やす",
+        english: "to burn (transitive)"
+      },
+      {
+        kanji: "氷{こおり}",
+        english: "ice"
+      },
+      {
+        kanji: "取{と}り替{か}える",
+        english: "to replace"
+      },
+    ],
+    translation: `A: Oh, that meat, hurry up and turn it over. This one and this one too.\nB: Wow, the meat burned! What should I do!\nA: Ah, in that case, just put this ice on the net. The net got dirty.Let's have it replaced.`,
+    audio: "T6.mp3",
+    topic: MiniStoryTopics.FOOD,
+    page: 17,
   },
 };
