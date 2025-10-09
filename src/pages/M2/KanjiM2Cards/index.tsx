ac20d248a56883ec24190dddde6019f8fa02732d
Felipe Zeba <felipe_zeba@outlook.com>
2025-10-09 19:32:51 +0900

    refactor: change parameter to receive words list instead of indexes

--- src/pages/M2/KanjiM2Cards/index.tsx
+++ src/pages/M2/KanjiM2Cards/index.tsx
@@ -1,18 +1,14 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
-import M2_KANJI from "../../../consts/kanji/M2";
 import KanjiM2 from "./KanjiM2Cards";
 
 const KanjiM2CardsPage: React.FC = () => {
   const [searchParams] = useSearchParams(); 
-  const startKanjiIdParam : number = parseInt(searchParams.get("startKanjiId")!) || 426;
-  const endKanjiIdParam : number = parseInt(searchParams.get("endKanjiId")!) || 426;
-  const wordsList: TKanjiM2Words[] | TKanjiM2WordsWithExample[] = Object.values(M2_KANJI).filter(kaniItem => {
-    if (kaniItem.id < startKanjiIdParam || kaniItem.id > endKanjiIdParam) {
-      return false;
-    }
-    return true;
-  }).flatMap(item => item.words.concat(item.otherWords.map(word => ({...word, exampleSentenceRuby: ""}))));
+  const wordsListParam : Record<number, TKanjiM2> = JSON.parse(searchParams.get("wordsList") || "{}");
+  const wordsList: TKanjiM2WordsWithExample[] = Object.values(wordsListParam)
+    .flatMap(item => 
+      item.words.concat(item.otherWords.map(word => ({...word, exampleSentenceRuby: ""})))
+    );
 
   return (
     <KanjiM2 wordsList={wordsList} />
