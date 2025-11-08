ecc62c1779a880b23b2385e10ded3cb28ab5599d
Felipe Zeba <felipe_zeba@outlook.com>
2025-11-08 21:11:29 +0900

    feat: add nihongo500 N3 book questions 1-125

---
+++ src/consts/JLPT/N3/exercises/moji.const.ts
@@ -0,0 +1,438 @@
+export const JLPT_N3_MOJI_EXERCISES: JLPTExercise[] = [
+  {
+    id: 1,
+    content: [
+      '',
+      'の近くに引っ越したので便利です。',
+    ],
+    moji: '郵便局',
+    options: ['ゆうびんきょく', 'ゆびんきょく', 'ようべんきょく', 'よべんきょく'],
+    answer: ' ゆうびんきょく',
+  },
+  {
+    id: 4,
+    content: [
+      '初めまして。リンと',
+      '。',
+    ],
+    moji: 'もうします',
+    options: ['由します', '曲します', '申します', '直します'],
+    answer: ' xxxxx',
+  },
+  {
+    id: 7,
+    content: [
+      '財布が落ちていたので、交番に',
+      '。',
+    ],
+    moji: '届けた',
+    options: ['つづけた', ' あずけた', 'とどけた', 'ほどけた'],
+    answer: ' xxxxx',
+  },
+  {
+    id: 10,
+    content: [
+      '向こうの和室でお茶と',
+      'した。'
+    ],
+    moji: 'おかし',
+    options: ['お菓子', 'お果物', 'お果子', 'お菓物'],
+    answer: ' xxxxx',
+  },
+  {
+    id: 13,
+    content: [
+      '昔は、車の代わりに馬や牛が',
+      'を運んだりした。'
+    ],
+    moji: '荷物',
+    options: ['みもつ', 'かもつ', 'にもつ', 'いもつ'],
+    answer: ' xxxxx',
+  },
+  {
+    id: 16,
+    content: [
+      '次の信号を左に',
+      'ください。'
+    ],
+    moji: '曲がって',
+    options: ['もがって', 'みがって', 'むがって', 'まがって'],
+    answer: ' xxxxx',
+  },
+  {
+    id: 19,
+    content: [
+      'まもなく電車が',
+      '。危ないですから、黄色い線まで下がってお待ちください。'
+    ],
+    moji: 'まいります',
+    options: ['入ります', '参ります', '着ります', '回ります'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 22,
+    content: [
+      'やり方は簡単です。心配は',
+      '。'
+    ],
+    moji: '要りません',
+    options: ['ありません', 'おりません', 'たりません', 'いりません'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 25,
+    content: [
+      'この',
+      'は、辞書があれば読める。'
+    ],
+    moji: 'ざっし',
+    options: ['冊子', '雜誌', '冊誌', '雜子'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 28,
+    content: [
+      '',
+      'を机の上に置いてきてしまった。'
+    ],
+    moji: '宿題',
+    options: ['すくだい', 'しょくだい', 'しゅくだい', 'しくだい'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 31,
+    content: [
+      'これは夫がかいた',
+      'の絵です。'
+    ],
+    moji: '港',
+    options: ['やまと', 'みやこ', 'ちまた', 'みなと'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 34,
+    content: [
+      '電車が事故で',
+      '。'
+    ],
+    moji: 'おくれました',
+    options: ['割れました', '送れました', '遅れました', '別れました'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 37,
+    content: [
+      '彼は',
+      'に出ませんでした。'
+    ],
+    moji: '卒業式',
+    options: ['そっぎょうしき', 'そちぎょうしき', 'そつぎょうしき', 'そうぎょうしき'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 40,
+    content: [
+      '紹介します。',
+      'と息子です。'
+    ],  
+    moji: 'つま',
+    options: ['夫', '妻', '主人', '家内'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 43,
+    content: [
+      '',
+      'の予定が変わった。'
+    ],  
+    moji: '授業',
+    options: ['じぎょう', 'ずぎょう', 'じゅうぎょう', 'じゅぎょう'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 46,
+    content: [
+      '彼女と結婚の',
+      'をした。'
+    ],  
+    moji: '約束',
+    options: ['よくそく', 'よっそく', 'やくそく', 'やっそく'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 49,
+    content: [
+      '船より',
+      'のほうが速い。'
+    ],  
+    moji: 'ひこうき',
+    options: ['飛高機', '飛行機', '引行機', '引高機'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 52,
+    content: [
+      '大学で',
+      'を勉強しています。'
+    ],  
+    moji: '美術',
+    options: ['ぎじつ', 'ぎじゅつ', 'びじつ', 'びじゅつ'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 55,
+    content: [
+      '橋を',
+      '、二つ目の角を右へ曲がると郵便局があります。'
+    ],  
+    moji: 'わたって',
+    options: ['渡って', '通って', '進って', '沿って'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 58,
+    content: [
+      '',
+      'ですが、パーティーに出席できません。'
+    ],  
+    moji: '残念',
+    options: ['さんれん', 'ざんれん', 'ざっねん', 'ざんねん'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 61,
+    content: [
+      '自動販売機を使いたいので、1万円札を',
+      'してくれませんか。'
+    ],  
+    moji: '細かく',
+    options: ['こまかく', 'みじかく', 'ほそかく', 'やわらかく'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 64,
+    content: [
+      'うちは普通の家ですが、世界各国からいろんな人が',
+      'にきます。'
+    ],  
+    moji: 'とまり',
+    options: ['足まり', '泊まり', '通まり', '止まり'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 67,
+    content: [
+      '店をきれいにしたら、以前に',
+      '、女性客が増えた。'
+    ],  
+    moji: '比べて',
+    options: ['ひらべて', 'くらべて', 'ならべて', 'こらべて'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 70,
+    content: [
+      '彼女はいつも助けてくれる',
+      '人です。'
+    ],  
+    moji: 'やさしい',
+    options: ['難しい', '易しい', '優しい', '美しい'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 73,
+    content: [
+      '',
+      '、面接試験を受けます。'
+    ],  
+    moji: '再来週',
+    options: ['さらいしゅう', 'さいらいしゅう', 'せらいしゅう', 'せいらいしゅう'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 76,
+    content: [
+      '',
+      'は熱心な学生だから、きっと試験に合格するでしょう。'
+    ],  
+    moji: '君',
+    options: ['くん', 'あなた', 'かみ', 'きみ'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 79,
+    content: [
+      '石けんが12',
+      '、入っています。'
+    ],  
+    moji: 'こ',
+    options: ['個', '台', '校', '故'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 82,
+    content: [
+      'あのお寺の庭は、みどりが多くて',
+      'です。'
+    ],  
+    moji: '静か',
+    options: ['にぎやか', 'ゆたか', 'しずか', 'のどか'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 85,
+    content: [
+      '皆様によろしく',
+      'ください。'
+    ],  
+    moji: 'お伝え',
+    options: ['おつだえ', 'おつたえ', 'おだつえ', 'おたつえ'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 88,
+    content: [
+      '公園で',
+      'と遊んだ。'
+    ],  
+    moji: 'こども',
+    options: ['子達', '子供', '小児', '小人'],
+    answer: 'xxxxx',
+  },
+  /*
+  * REVIEW OF SECTION 1
+  * The following exercises are for review purposes.
+  */
+  {
+    id: 91,
+    content: [
+      'バスで',
+      'へ行く。'
+    ],  
+    moji: '空港',
+    options: ['くうこう', 'こうくう'],
+    answer: 'xxxxx',
+    refersTo: 31,
+  },
+  {
+    id: 92,
+    content: [
+      '午後は',
+      'に行動して下さい。'
+    ],  
+    moji: 'じゆう',
+    options: ['自由', '理由'],
+    answer: 'xxxxx',
+    refersTo: 4,
+  },
+  {
+    id: 97,
+    content: [
+      '授業に',
+      'ようにしてください。'
+    ],  
+    moji: '遅れない',
+    options: ['おそれない', 'おくれない'],
+    answer: 'xxxxx',
+    refersTo: 34,
+  },
+  {
+    id: 98,
+    content: [
+      'こんな事があるなんて、',
+      '。'
+    ],  
+    moji: 'しんじられない',
+    options: ['信じられない', '真じられない'],
+    answer: 'xxxxx',
+  },
+  {
+    id: 103,
+    content: [
+      '',
+      'が好きです。'
+    ],  
+    moji: '果物',
+    options: ['くだもの', 'こどもの'],
+    answer: 'xxxxx',
+    refersTo: 10,
+  },
+  {
+    id: 104,
+    content: [
+      'この',
+      'を聞くと国を思い出す。'
+    ],  
+    moji: 'きょく',
+    options: ['局', '曲'],
+    answer: 'xxxxx',
+    refersTo: 16,
+  },
+  {
+    id: 109,
+    content: [
+      'ここはお年寄りのための',
+      'です。'
+    ],  
+    moji: '優先席',
+    options: ['ようせんせき', 'ゆうせんせき'],
+    answer: 'xxxxx',
+    refersTo: 70,
+  },
+  {
+    id: 110,
+    content: [
+      'お皿を',
+      'しまった。'
+    ],  
+    moji: 'わって',
+    options: ['割って', '折って'],
+    answer: 'xxxxx',
+    refersTo: 34,
+  },
+  {
+    id: 115,
+    content: [
+      '',
+      'で旅行に行く。'
+    ],  
+    moji: '夫婦',
+    options: ['ふさい', 'ふうふ'],
+    answer: 'xxxxx',
+    refersTo: 31,
+  },
+  {
+    id: 116,
+    content: [
+      '鳥が',
+      'でいる。'
+    ],  
+    moji: 'とんで',
+    options: ['込んで', '飛んで'],
+    answer: 'xxxxx',
+    refersTo: 49,
+  },
+  {
+    id: 121,
+    content: [
+      '紙を',
+      'におります。'
+    ],  
+    moji: '三角',
+    options: ['さんかく', 'みつかど'],
+    answer: 'xxxxx',
+    refersTo: 55,
+  },
+  {
+    id: 122,
+    content: [
+      '旅館に',
+      '。'
+    ],  
+    moji: 'とまる',
+    options: ['泊まる', '宿まる'],
+    answer: 'xxxxx',
+    refersTo: 64,
+  },
+];
+
