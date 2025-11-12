a7e053fb624bfe18ba1d2b7f94ecfc5e58ee053f
Felipe Zeba <felipe_zeba@outlook.com>
2025-11-12 20:52:19 +0900

    feat: add NihongoNoMori N3 moji-goi 100-300

--- src/consts/moji_goi/NihongoNoMori.const.ts
+++ src/consts/moji_goi/NihongoNoMori.const.ts
@@ -433,4 +433,949 @@ export const NIHONGO_NO_MORI_WORDS = [
     meaning: "Double/(number of)times",
     example: "今日店員が少ないので、いつもの倍頑張ろう。"
   },
+  {
+    id: 102,
+    word: "羊",
+    reading: "ひつじ",
+    meaning: "Sheep",
+    example: "羊を飼う。"
+  },
+  {
+    id: 103,
+    word: "暇",
+    reading: "ひま",
+    meaning: "Free time",
+    example: "今週末は暇です。"
+  },
+  {
+    id: 105,
+    word: "袋",
+    reading: "ふくろ",
+    meaning: "Bag",
+    example: "お菓子を袋に入れる。"
+  },
+  {
+    id: 107,
+    word: "星",
+    reading: "ほし",
+    meaning: "Star",
+    example: "山に星を見に行く。"
+  },
+  {
+    id: 108,
+    word: "骨",
+    reading: "ほね",
+    meaning: "Bone",
+    example: "骨が折れる。"
+  },
+  {
+    id: 109,
+    word: "棒",
+    reading: "ぼう",
+    meaning: "Stick",
+    example: "棒で叩く。"
+  },
+  {
+    id: 110,
+    word: "僕",
+    reading: "ぼく",
+    meaning: "I (used by males)",
+    example: "僕はアニメを見るのが好きだ。"
+  },
+  {
+    id: 111,
+    word: "孫",
+    reading: "まご",
+    meaning: "Grandchild",
+    example: "孫と遊ぶのが、私の夢だ。"
+  },
+  {
+    id: 113,
+    word: "街",
+    reading: "まち",
+    meaning: "Town/street",
+    example: "東京の街は夜になっても明るい。"
+  },
+  {
+    id: 114,
+    word: "窓",
+    reading: "まど",
+    meaning: "Window",
+    example: "窓を閉める。"
+  },
+  {
+    id: 116,
+    word: "湖",
+    reading: "みずうみ",
+    meaning: "Lake",
+    example: "湖に鳥がたくさんいる。"
+  },
+  {
+    id: 117,
+    word: "緑",
+    reading: "みどり",
+    meaning: "Green",
+    example: "緑色のTシャツを着る。"
+  },
+  {
+    id: 118,
+    word: "港",
+    reading: "みなと",
+    meaning: "Port",
+    example: "港から船がでる。"
+  },
+  {
+    id: 121,
+    word: "胸",
+    reading: "むね",
+    meaning: "Chest",
+    example: "胸が苦しい。"
+  },
+  {
+    id: 123,
+    word: "面",
+    reading: "めん",
+    meaning: "Side・surface",
+    example: "この面を下にする。"
+  },
+  {
+    id: 130,
+    word: "指",
+    reading: "ゆび",
+    meaning: "Finger",
+    example: "紙で指を切った。"
+  },
+  {
+    id: 135,
+    word: "列",
+    reading: "れつ",
+    meaning: "Row・line",
+    example: "列になって歩く。"
+  },
+  {
+    id: 136,
+    word: "合図",
+    reading: "あいず",
+    meaning: "Signal・sign",
+    example: "他の人に気づかれないように合図する。"
+  },
+  {
+    id: 144,
+    word: "以降",
+    reading: "いこう",
+    meaning: "After・from now on",
+    example: "21時以降は外出してはいけません。"
+  },
+  {
+    id: 145,
+    word: "維持",
+    reading: "いじ [する]",
+    meaning: "Maintenance・preservation",
+    example: "今の体重を維持するために、毎日運動している。"
+  },
+  {
+    id: 146,
+    word: "意識",
+    reading: "いしき [する]",
+    meaning: "Consciousness・awareness",
+    example: "あまりのショックに意識を失った。"
+  },
+  {
+    id: 150,
+    word: "位置",
+    reading: "いち",
+    meaning: "Position・location",
+    example: "机の位置を変える。"
+  },
+  {
+    id: 151,
+    word: "一流",
+    reading: "いちりゅう",
+    meaning: "First class・top grade",
+    example: "彼の料理は、一流だ。"
+  },
+  {
+    id: 153,
+    word: "一般",
+    reading: "いっぱん",
+    meaning: "General・ordinary",
+    example: "新しい映画を一般の人に見せる。"
+  },
+  {
+    id: 154,
+    word: "移動",
+    reading: "いどう [する]",
+    meaning: "Move・transfer",
+    example: "場所を移動する。"
+  },
+  {
+    id: 155,
+      word: "以内",
+      reading: "いない",
+      meaning: "Within",
+      example: "宿題は、2時間以内に出してください。"
+  },
+  {
+    id: 156,
+    word: "田舎",
+    reading: "いなか",
+    meaning: "Countryside",
+    example: "田舎で生活する。"
+  },
+  {
+    id: 157,
+    word: "居間",
+    reading: "いま",
+    meaning: "Living room",
+    example: "居間で家族と話す。"
+  },
+  {
+    id: 159,
+    word: "印刷",
+    reading: "いんさつ",
+    meaning: "Printing・Print",
+    example: "教科書を印刷する。"
+  },
+  {
+    id: 160,
+    word: "印象",
+    reading: "いんしょう",
+    meaning: "Impression",
+    example: "印象がよい。"
+  },
+  {
+    id: 164,
+    word: "右折",
+    reading: "うせつ [する]",
+    meaning: "Right turn",
+    example: "次の角を右折してください。"
+  },
+  {
+    id: 168,
+    word: "影響",
+    reading: "えいきょう [する]",
+    meaning: "Influence",
+    example: "大雪の影響で、電車が止まっている。"
+  },
+  {
+    id: 169,
+    word: "営業",
+    reading: "えいぎょう [する]",
+    meaning: "Business・sales",
+    example: "森社へ 営業に行く。"
+  },
+  {
+    id: 170,
+    word: "栄養",
+    reading: "えいよう",
+    meaning: "Nutrition",
+    example: "野菜を食べて、栄養を取る。"
+  },
+  {
+    id: 173,
+    word: "延期",
+    reading: "えんき [する]",
+    meaning: "Postponement・delay",
+    example: "雨のため、今日の試合を延期します。"
+  },
+  {
+    id: 174,
+    word: "演奏",
+    reading: "えんそう [する]",
+    meaning: "Performance・playing (a musical instrument)",
+    example: "楽器を演奏する。"
+  },
+  {
+    id: 175,
+    word: "遠慮",
+    reading: "えんりょ [する]",
+    meaning: "Restraint・Precaution",
+    example: "この場所での通話は、ご遠慮ください。"
+  },
+  {
+    id: 176,
+    word: "応援",
+    reading: "おうえん [する]",
+    meaning: "Support・cheering",
+    example: "友達を応援する。"
+  },
+  {
+    id: 177,
+    word: "横断",
+    reading: "おうだん [する]",
+    meaning: "Crossing",
+    example: "車で、日本を横断する。"
+  },
+  {
+    id: 178,
+    word: "往復",
+    reading: "おうふく [する]",
+    meaning: "Round trip",
+    example: "家と駅を往復する。"
+  },
+  {
+    id: 179,
+    word: "応募",
+    reading: "おうぼ [する]",
+    meaning: "Application・entry",
+    example: "アルバイトに応募する。"
+  },
+  {
+    id: 180,
+    word: "応用",
+    reading: "おうよう [する]",
+    meaning: "Application・practical use",
+    example: "習った文法を応用する。"
+  },
+  {
+    id: 181,
+    word: "大勢",
+    reading: "おおぜい",
+    meaning: "Many people・crowd",
+    example: "日本語を勉強している人が大勢いる。"
+  },
+  {
+    id: 182,
+    word: "大家",
+    reading: "おおや",
+    meaning: "Landlord・householder",
+    example: "私の家の大家はとてもやさしい。"
+  },
+  {
+    id: 183,
+    word: "親子",
+    reading: "おやこ",
+    meaning: "Parent and child",
+    example: "親子で参加する。"
+  },
+  {
+    id: 185,
+    word: "温泉",
+    reading: "おんせん",
+    meaning: "Hot spring",
+    example: "友達と温泉に行く。"
+  },
+  {
+    id: 187,
+    word: "開園",
+    reading: "かいえん",
+    meaning: "Open (park, garden, zoo, etc.)",
+    example: "この動物園は、朝9時に開園する。"
+  },
+  {
+    id: 188,
+    word: "絵画",
+    reading: "かいが",
+    meaning: "Painting",
+    example: "世界の絵画を集めるのが好きだ。"
+  },
+  {
+    id: 190,
+    word: "海岸",
+    reading: "かいがん",
+    meaning: "Coast・shore",
+    example: "海岸を歩く。"
+  },
+  {
+    id: 193,
+    word: "解決",
+    reading: "かいけつ [する]",
+    meaning: "Solution・resolution",
+    example: "話し合って問題を解決する。"
+  },
+  {
+    id: 194,
+    word: "改札",
+    reading: "かいさつ",
+    meaning: "Ticket gate",
+    example: "改札の前で待ち合わせをする。"
+  },
+  {
+    id: 195,
+    word: "開始",
+    reading: "かいし",
+    meaning: "Start・beginning",
+    example: "テスト開始10分前にお腹が痛くなった。"
+  },
+  {
+    id: 196,
+    word: "回収",
+    reading: "かいしゅう [する]",
+    meaning: "Collect・retrieval",
+    example: "ごみを回収してもらう。"
+  },
+  {
+    id: 197,
+    word: "開場",
+    reading: "かいじょう [する]",
+    meaning: "Opening (of a venue)",
+    example: "開場の時間まで、コーヒーを飲みながら待つ。"
+  },
+  {
+    id: 198,
+    word: "回数",
+    reading: "かいすう",
+    meaning: "Number of times",
+    example: "今までに仕事を変えた回数が多い。"
+  },
+  {
+    id: 199,
+    word: "快晴",
+    reading: "かいせい",
+    meaning: "Clear weather・Sunny",
+    example: "快晴の日が続いている。"
+  },
+  {
+    id: 200,
+    word: "解説",
+    reading: "かいせつ [する]",
+    meaning: "Explanation・Commentary",
+    example: "問題の答えを解説する。"
+  },
+  {
+    id: 201,
+    word: "階段",
+    reading: "かいだん",
+    meaning: "Stairs・staircase",
+    example: "昨日階段から落ちてけがをした。"
+  },
+  {
+    id: 202,
+    word: "開店",
+    reading: "かいてん [する]",
+    meaning: "Opening (of a store)",
+    example: "この店は、朝5時から開店している。"
+  },
+  {
+    id: 204,
+    word: "価格",
+    reading: "かかく",
+    meaning: "Price・cost",
+    example: "この絵の価格は、50万円です。"
+  },
+  {
+    id: 205,
+    word: "科学",
+    reading: "かがく",
+    meaning: "Science",
+    example: "科学の授業を受ける。"
+  },
+  {
+    id: 206,
+    word: "家具",
+    reading: "かぐ",
+    meaning: "Furniture",
+    example: "家具を新しくする。"
+  },
+  {
+    id: 207,
+    word: "確認",
+    reading: "かくにん [する]",
+    meaning: "Confirmation・verification",
+    example: "終わった宿題を確認する。"
+  },
+  {
+    id: 209,
+    word: "加工",
+    reading: "かこう [する]",
+    meaning: "Processing・manufacturing",
+    example: "写真に写った自分の顔を加工する。"
+  },
+  {
+    id: 210,
+    word: "火災",
+    reading: "かさい",
+    meaning: "Fire (disaster)",
+    example: "家の近くで火災が発生した。"
+  },
+  {
+    id: 213,
+    word: "下線",
+    reading: "かせん",
+    meaning: "Underline",
+    example: "大事なところに下線を引く。"
+  },
+  {
+    id: 214,
+    word: "花壇",
+    reading: "かだん",
+    meaning: "Flower bed",
+    example: "花壇に花を植える。"
+  },
+  {
+    id: 215,
+    word: "価値",
+    reading: "かち",
+    meaning: "Value",
+    example: "この本は読む価値がある。"
+  },
+  {
+    id: 218,
+    word: "仮定",
+    reading: "かてい",
+    meaning: "Assumption・hypothesis",
+    example: "日本で働くと仮定して、これからについて考える。"
+  },
+  {
+    id: 219,
+    word: "間隔",
+    reading: "かんかく",
+    meaning: "Interval・spacing",
+    example: "となりの人と間隔をあけて座る。"
+  },
+  {
+    id: 220,
+    word: "感覚",
+    reading: "かんかく",
+    meaning: "Sense・feeling",
+    example: "寒さを感じる感覚が鈍くなる。"
+  },
+  {
+    id: 221,
+    word: "観客",
+    reading: "かんきゃく",
+    meaning: "Audience・spectator",
+    example: "今日は雨なので、観客が少ない。"
+  },
+  {
+    id: 222,
+    word: "環境",
+    reading: "かんきょう",
+    meaning: "Environment",
+    example: "環境について考える。"
+  },
+  {
+    id: 223,
+    word: "関係",
+    reading: "かんけい",
+    meaning: "Relation・relationship",
+    example: "日本語に関係する仕事をする。"
+  },
+  {
+    id: 224,
+    word: "歓迎",
+    reading: "かんげい [する]",
+    meaning: "Welcome・reception",
+    example: "彼女を歓迎する。"
+  },
+  {
+    id: 225,
+    word: "感激",
+    reading: "かんげき [する]",
+    meaning: "Deep emotion・appreciation",
+    example: "それは、私が一番感激した映画です。"
+  },
+  {
+    id: 226,
+    word: "観光",
+    reading: "かんこう [する]",
+    meaning: "Sightseeing・tourism",
+    example: "東京を観光する。"
+  },
+  {
+    id: 227,
+    word: "観察",
+    reading: "かんさつ [する]",
+    meaning: "Observation・watching",
+    example: "花を観察する。"
+  },
+  {
+    id: 229,
+    word: "感謝",
+    reading: "かんしゃ [する]",
+    meaning: "Gratitude・thanks",
+    example: "感謝の気持ちを伝える。"
+  },
+  {
+    id: 230,
+    word: "患者",
+    reading: "かんじゃ",
+    meaning: "Patient (person)",
+    example: "この病院に入院している患者は少ない。"
+  },
+  {
+    id: 231,
+    word: "感心",
+    reading: "かんしん [する]",
+    meaning: "Admiration・being impressed",
+    example: "彼の勉強方法に感心する。"
+  },
+  {
+    id: 232,
+    word: "関心",
+    reading: "かんしん",
+    meaning: "Interest・concern",
+    example: "日本の文化に関心がある。"
+  },
+  {
+    id: 233,
+    word: "完成",
+    reading: "かんせい [する]",
+    meaning: "Completion・finish",
+    example: "新しい家は、明日完成する。"
+  },
+  {
+    id: 234,
+    word: "感想",
+    reading: "かんそう [する]",
+    meaning: "Impression・thoughts",
+    example: "この本を読んだ感想を伝える。"
+  },
+  {
+    id: 235,
+    word: "乾燥",
+    reading: "かんそう [する]",
+    meaning: "Drying",
+    example: "服を乾燥させる。"
+  },
+  {
+    id: 236,
+    word: "感動",
+    reading: "かんどう [する]",
+    meaning: "Deep emotion・being moved・being impressed",
+    example: "彼の言葉に感動した。"
+  },
+  {
+    id: 237,
+    word: "看板",
+    reading: "かんばん",
+    meaning: "Signboard・billboard",
+    example: "黄色の看板を右に曲がる。"
+  },
+  {
+    id: 238,
+    word: "外出",
+    reading: "がいしゅつ [する]",
+    meaning: "Going out・outing",
+    example: "母親と一緒に外出する。"
+  },
+  {
+    id: 239,
+    word: "外食",
+    reading: "がいしょく [する]",
+    meaning: "Eating out",
+    example: "今日は、家族で外食した。"
+  },
+  {
+    id: 240,
+    word: "楽器",
+    reading: "がっき",
+    meaning: "Musical instrument",
+    example: "様々な楽器の音が聞こえる。"
+  },
+  {
+    id: 241,
+    word: "我慢",
+    reading: "がまん [する]",
+    meaning: "Being patient・endurance",
+    example: "おかしを食べるのを我慢する。"
+  },
+  {
+    id: 243,
+    word: "機械",
+    reading: "きかい",
+    meaning: "Machine",
+    example: "機械を新しくする。"
+  },
+  {
+    id: 244,
+    word: "機会",
+    reading: "きかい",
+    meaning: "Opportunity・chance",
+    example: "尊敬している選手に会う機会があった。"
+  },
+  {
+    id: 247,
+    word: "機関",
+    reading: "きかん",
+    meaning: "Organization・institution",
+    example: "将来は国の機関で働きたい。"
+  },
+  {
+    id: 248,
+    word: "企業",
+    reading: "きぎょう",
+    meaning: "Enterprise・business・company",
+    example: "日本の企業で働く。"
+  },
+  {
+    id: 249,
+    word: "期限",
+    reading: "きげん",
+    meaning: "Deadline・time limit",
+    example: "宿題を出す期限が過ぎてしまった。"
+  },
+  {
+    id: 252,
+    word: "季節",
+    reading: "きせつ",
+    meaning: "Season",
+    example: "日本には4つの季節がある。"
+  },
+  {
+    id: 253,
+    word: "基礎",
+    reading: "きそ",
+    meaning: "Foundation・basis",
+    example: "日本語の基礎をしっかり勉強する。"
+  },
+  {
+    id: 254,
+    word: "規則",
+    reading: "きそく",
+    meaning: "Rule・regulation",
+    example: "規則をまもって生活する。"
+  },
+  {
+    id: 255,
+    word: "期待",
+    reading: "きたい [する]",
+    meaning: "Expectation・anticipation・To expect",
+    example: "彼は多くの人から期待されている。"
+  },
+  {
+    id: 256,
+    word: "帰宅",
+    reading: "きたく [する]",
+    meaning: "Returning home",
+    example: "毎日6時に帰宅する。"
+  },
+  {
+    id: 257,
+    word: "機長",
+    reading: "きちょう",
+    meaning: "Captain (of a ship or aircraft)",
+    example: "彼は来月から機長になる。"
+  },
+  {
+    id: 258,
+    word: "切手",
+    reading: "きって",
+    meaning: "Postage stamp",
+    example: "切手を買いに行く。"
+  },
+  {
+    id: 259,
+    word: "記念",
+    reading: "きねん",
+    meaning: "Commemoration・anniversary",
+    example: "記念に家族で写真を撮る。"
+  },
+  {
+    id: 265,
+    word: "休講",
+    reading: "きゅうこう [する]",
+    meaning: "Class cancellation・No lecture",
+    example: "今日の大学の授業は休講になった。"
+  },
+  {
+    id: 267,
+    word: "給料",
+    reading: "きゅうりょう",
+    meaning: "Salary・wage",
+    example: "初めての給料で両親にプレゼントを買う。"
+  },
+  {
+    id: 268,
+    word: "教育",
+    reading: "きょういく",
+    meaning: "Education",
+    example: "子供を教育する。"
+  },
+  {
+    id: 269,
+    word: "教室",
+    reading: "きょうしつ",
+    meaning: "Classroom",
+    example: "教室で遊ぶ。"
+  },
+  {
+    id: 270,
+    word: "競争",
+    reading: "きょうそう",
+    meaning: "Competition",
+    example: "どちらが早く走れるか競争する。"
+  },
+  {
+    id: 271,
+    word: "兄弟",
+    reading: "きょうだい",
+    meaning: "Siblings",
+    example: "私は、兄弟が3人います。"
+  },
+  {
+    id: 272,
+    word: "共通",
+    reading: "きょうつう",
+    meaning: "Common・similarity",
+    example: "彼とは共通の趣味があります。"
+  },
+  {
+    id: 273,
+    word: "共同",
+    reading: "きょうどう [する]",
+    meaning: "Cooperation・collaboration",
+    example: "共同で使用する。"
+  },
+  {
+    id: 275,
+    word: "協力",
+    reading: "きょうりょく [する]",
+    meaning: "Cooperation・collaboration",
+    example: "みんなと協力してごみを拾う。"
+  },
+  {
+    id: 277,
+    word: "距離",
+    reading: "きょり",
+    meaning: "Distance",
+    example: "学校まで距離がある。"
+  },
+  {
+    id: 278,
+    word: "記録",
+    reading: "きろく [する]",
+    meaning: "Record・documentation",
+    example: "今までのテストの点数を記録する。"
+  },
+  {
+    id: 279,
+    word: "禁煙",
+    reading: "きんえん",
+    meaning: "No smoking",
+    example: "健康のために禁煙する。"
+  },
+  {
+    id: 280,
+    word: "禁止",
+    reading: "きんし [する]",
+    meaning: "Prohibition・Not allowed・to prohibit",
+    example: "3週間外に出ることを禁止する。"
+  },
+  {
+    id: 281,
+    word: "近所",
+    reading: "きんじょ",
+    meaning: "Neighborhood",
+    example: "近所の公園で遊ぶ。"
+  },
+  {
+    id: 282,
+    word: "緊張",
+    reading: "きんちょう [する]",
+    meaning: "Nervousness・tension",
+    example: "みんなの前で話すのは緊張する。"
+  },
+  {
+    id: 283,
+    word: "技術",
+    reading: "ぎじゅつ",
+    meaning: "Technique・skill",
+    example: "彼の技術は素晴らしい。"
+  },
+  {
+    id: 284,
+    word: "牛乳",
+    reading: "ぎゅうにゅう",
+    meaning: "Milk",
+    example: "毎朝牛乳を飲んでから学校へ行く。"
+  },
+  {
+    id: 285,
+    word: "銀行",
+    reading: "ぎんこう",
+    meaning: "Bank",
+    example: "銀行にお金を預ける。"
+  },
+  {
+    id: 286,
+    word: "空気",
+    reading: "くうき",
+    meaning: "Air・atmosphere",
+    example: "きれいな空気を吸う。"
+  },
+  {
+    id: 287,
+    word: "空港",
+    reading: "くうこう",
+    meaning: "Airport",
+    example: "空港で待ち合わせをする。"
+  },
+  {
+    id: 288,
+    word: "草木",
+    reading: "くさき",
+    meaning: "Plants・vegetation",
+    example: "草木を植える。"
+  },
+  {
+    id: 289,
+    word: "草花",
+    reading: "くさばな",
+    meaning: "Flowers・plants",
+    example: "草花を買う。"
+  },
+  {
+    id: 291,
+    word: "工夫",
+    reading: "くふう [する]",
+    meaning: "Ingenuity",
+    example: "勉強方法を工夫する。"
+  },
+  {
+    id: 293,
+    word: "苦労",
+    reading: "くろう [する]",
+    meaning: "Hardship・trouble",
+    example: "苦労した経験がある。"
+  },
+  {
+    id: 294,
+    word: "訓練",
+    reading: "くんれん [する]",
+    meaning: "Training・practice",
+    example: "うまくなるには、訓練が必要だ。"
+  },
+  {
+    id: 295,
+    word: "具合",
+    reading: "ぐあい",
+    meaning: "Condition・state・health",
+    example: "体の具合が悪い。"
+  },
+  {
+    id: 296,
+    word: "経営",
+    reading: "けいえい [する]",
+    meaning: "Management・administration",
+    example: "経営を勉強する。"
+  },
+  {
+    id: 297,
+    word: "計画",
+    reading: "けいかく [する]",
+    meaning: "Plan・project",
+    example: "授業の計画を立てる。"
+  },
+  {
+    id: 298,
+    word: "敬語",
+    reading: "けいご",
+    meaning: "Honorific language",
+    example: "敬語を使えるように勉強する。"
+  },
+  {
+    id: 299,
+    word: "警察",
+    reading: "けいさつ",
+    meaning: "Police",
+    example: "警察に声をかけられた。"
+  },
+  {
+    id: 300,
+    word: "計算",
+    reading: "けいさん [する]",
+    meaning: "Calculation・To calculate",
+    example: "彼は、計算するのが早い。"
+  }
 ]
