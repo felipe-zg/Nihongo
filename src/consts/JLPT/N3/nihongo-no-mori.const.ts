4956187645f3897207856a24dc6ada7f01eaeabc
Felipe Zeba <felipe_zeba@outlook.com>
2025-11-14 19:27:38 +0900

    feat: add nihongonomori JLPT live words week 2

--- src/consts/JLPT/N3/nihongo-no-mori.const.ts
+++ src/consts/JLPT/N3/nihongo-no-mori.const.ts
@@ -148,4 +148,143 @@ export const NIHONGO_NO_MORI_JLPT_N3: T[] = [
     explanation: "Refers to the act of canceling something that was previously planned or arranged.",
     example: "予定が変わったので、美容院の予約を{取り消さなければならない}。I have to cancel my hair appointment."
   },
+  {
+    kanji: "折れる",
+    kana: "おれる",
+    english: "To break・To snap・To fold",
+    explanation: "Refers to the action of something breaking or snapping, or folding. (Usually used for things like branches, bones, etc.)",
+    example: "木の枝が折れた。The tree branch broke."
+  },
+  {
+    kanji: "複数",
+    kana: "ふくすう",
+    english: "Multiple・Plural",
+    explanation: "Refers to more than one or several items or people. (たさんと同じ意味だ）",
+    example: "ルールが複数あって、覚えるのが大変だ。There are multiple rules, and it's hard to remember them."
+  },
+  {
+    kanji: "増減",
+    kana: "ぞうげん［する］",
+    english: "Increase and decrease・To increase and decrease",
+    explanation: "Refers to the fluctuation or variation in numbers or amounts.",
+    example: "お金の増減が見えるようにする。To make the increase and decrease of money visible."
+  },
+  {
+    kanji: "焼く",
+    kana: "やく",
+    english: "To bake・To grill・To burn",
+    explanation: "Refers to the action of cooking food using dry heat, such as baking or grilling.",
+    example: "美味しいパンを焼いた。I baked delicious bread."
+  },
+  {
+    kanji: "燃える",
+    kana: "もえる",
+    english: "To burn・To be on fire",
+    explanation: "Refers to the action of something burning or being on fire.",
+    example: "木が燃えている。The tree is burning."
+  },
+  {
+    kanji: "積極的な",
+    kana: "せっきょくてき［な］",
+    english: "Positive・Active・Proactive",
+    explanation: "Refers to a proactive or positive attitude towards situations or tasks.",
+    example: "彼は仕事に対して積極的だ。He is proactive about his work."
+  },
+  {
+    kanji: "消極的な",
+    kana: "しょうきょくてき［な］",
+    english: "Negative・Passive",
+    explanation: "Refers to a passive or negative attitude towards situations or tasks.",
+    example: "私も彼も消極的なので、あまり一緒に話をしたことがない。Both he and I are passive, so we haven't talked much together."
+  },
+  {
+    kanji: "比較的な",
+    kana: "ひかくてき［な］",
+    english: "Comparative・Relatively",
+    explanation: "Refers to something that is considered in relation to something else, often indicating a relative degree or extent.",
+    example: "今日は比較的暑い。Today is relatively hot. (Compared to other days)"
+  },
+  {
+    kanji: "自動的な",
+    kana: "じどうてき［な］",
+    english: "Automatic・Automatically",
+    explanation: "Refers to something that operates or happens without human intervention.",
+    example: "このドアは自動的に開く。This door opens automatically."
+  },
+  {
+    kanji: "ぴったり",
+    kana: "ぴったり［する］",
+    english: "Perfectly・Exactly・To fit perfectly",
+    explanation: "Refers to something that fits perfectly or is exactly right.",
+    example: "体にぴったり合う服を買う。I buy clothes that fit my body perfectly."
+  },
+  {
+    kanji: "ばったり",
+    kana: "ばったり［する］",
+    english: "Suddenly・By chance・To meet by chance",
+    explanation: "Refers to something happening suddenly or unexpectedly, often used when meeting someone by chance.",
+    example: "駅前でばったりゆか先生に会ったんだよ。I met Yuka-sensei by chance (suddenly; without planning) in front of the station."
+  },
+  {
+    kanji: "さっぱり",
+    kana: "さっぱり 〜 ない",
+    english: "Not at all・Completely (not)",
+    explanation: "Used to express that something is not done at all or completely not the case. (ぜんぜん 〜 ない と同じ意味だ）",
+    example: "彼の話はさっぱり分からない。I don't understand his story at all."
+  },
+  {
+    kanji: "延期",
+    kana: "えんき［する］",
+    english: "Postponement・To postpone",
+    explanation: "Used when an event or activity is rescheduled to a later time or date.",
+    example: "参加できない人が多いので、今日の会議は延期された。Today's meeting was postponed because many people could not attend."
+  },
+  {
+    kanji: "遅延",
+    kana: "ちえん［する］",
+    english: "Delay・To delay",
+    explanation: "Refers to the act of being delayed or causing a delay, often used in the context of transportation or schedules. (来る時間に来ないこと）",
+    example: "電車が１０分遅延しました。The train was delayed by 10 minutes."
+  },
+  {
+    kanji: "延長",
+    kana: "えんちょう［する］",
+    english: "Extension・To extend",
+    explanation: "Refers to the act of extending the duration or length of something, such as a deadline or a stay. Can be used for things as well as time periods.",
+    example: "宿題の締め切りが一週間延長された。The homework deadline was extended by one week."
+  },
+  {
+    kanji: "早退",
+    kana: "そうたい［する］",
+    english: "Leaving early・To leave early",
+    explanation: "Refers to the act of leaving a place, such as work or school, before the scheduled time.",
+    example: "体調が悪いので、今日は早退します。I will leave early today because I am not feeling well."
+  },
+  {
+    kanji: "普段",
+    kana: "ふだん",
+    english: "Usually・Ordinarily",
+    explanation: "Refers to something that is done regularly or habitually in everyday life. (いつもと同じ意味だ）",
+    example: "普段は朝ご飯を食べない。I usually don't eat breakfast."
+  },{
+    kanji: "交通費",
+    kana: "こうつうひ",
+    english: "Transportation expenses",
+    explanation: "Refers to the costs associated with traveling, such as bus or train fares.",
+    example: "会社は毎月交通費を支給する。The company provides transportation expenses every month."
+  },
+  {
+    kanji: "支給",
+    kana: "しきゅう［する］",
+    english: "Provision・To provide",
+    explanation: "Refers to the act of supplying or providing something, such as money or resources.",
+    example: "会社から毎月交通費が支給された。The company provided transportation expenses every month."
+  },
+  {
+    kanji: "順番",
+    kana: "じゅんばん",
+    english: "Order・Turn",
+    explanation: "Refers to the sequence or arrangement of things or people.",
+    example: "順番が来るまで、このいすにおかけになってお待ちください。Please sit in this chair and wait until your turn comes."
+  }
 ]
