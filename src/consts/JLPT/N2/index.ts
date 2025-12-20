ce1768aa5ec856e80b0a11908676e012e8ab1fe0
Felipe Zeba <felipe_zeba@outlook.com>
2025-12-20 17:10:32 +0900

    feat: add N2 nihongonomori moji-goi

---
+++ src/consts/JLPT/N2/index.ts
@@ -0,0 +1,5 @@
+export * from "./mojigoi.const";
+export * from "./nihongo-no-mori.const";
+export * from "./vocabs.const";
+export * from "./exercises/grammar.const";
+export * from "./exercises/moji.const";
