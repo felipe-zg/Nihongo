426580dd6a71cf5d1f8019b29cd5d717fd91e153
Felipe Zeba <felipe_zeba@outlook.com>
2025-12-29 00:42:08 +0900

    refactor: update text layout

--- src/pages/Music/components/RubyWord.tsx
+++ src/pages/Music/components/RubyWord.tsx
@@ -3,12 +3,15 @@ import { Text, VStack } from "native-base";
 export const RubyWord = ({ kanji, furigana }: RubyPart) => (
   <VStack alignItems="center">
     <Text
-      fontSize={{base: "md", md: "xl"}}
+      fontSize={{base: "md", md: "3xl"}}
       lineHeight="xs"
       opacity={furigana ? 1 : 0} // Hide but keep space
+      color="red.500"
+      bold
+      my={5}
     >
       {furigana || "あ"} {/* Filler kana for consistent height */}
     </Text>
-    <Text fontSize={{base: "2xl", md: "3xl"}}>{kanji}</Text>
+    <Text color={furigana ? "red.500" : "black"} fontSize={{base: "2xl", md: "6xl"}}>{kanji}</Text>
   </VStack>
 );
