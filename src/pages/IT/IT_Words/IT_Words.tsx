fcfbabd673e94a2f9de082824f136d21c0f0e19d
Felipe Zeba <felipe_zeba@outlook.com>
2026-09-08 07:50:45 +0900

    refactor: update ITWords page UI

--- src/pages/IT/IT_Words/IT_Words.tsx
+++ src/pages/IT/IT_Words/IT_Words.tsx
@@ -1,13 +1,35 @@
 import React from "react";
-import { Box } from "native-base";
+import { Box, Divider, HStack, Text } from "native-base";
+import { Word } from "../../FastPass/components/Word/Word";
 
-type Props = {};
+type Props = {
+  words: any[]; // Replace 'any' with the actual type of your words when available
+};
 
-const ITWords: React.FC<Props> = () => {
+const ITWords: React.FC<Props> = ({ words }) => {
 
   return (
-    <Box alignItems="center" mt={10}>
-      IT words page
+    <Box mx={10} alignItems="center" mt={10} borderWidth={1} borderColor="gray.300" p={4} borderRadius={8}>
+      {words.map((word) => (
+        <Box key={word.id} mb={4} w="100%">
+          <HStack flex={1} space={2} alignItems="flex-end">
+            <Word ruby={word.wordRuby} showFurigana={true} fontSize="2xl" color="yellow.400" />
+            <Text fontSize="md" color="tertiary.400">{word.meaning}</Text>
+          </HStack>
+          {word.extraVocabulary && word.extraVocabulary.length > 0 && (
+            <Box flex={2} mt={2}>
+              {word.extraVocabulary.map((extra: { wordRuby: string; meaning: string; }, index: React.Key | null | undefined) => (
+                <HStack space={2} alignItems="center">
+                  <Word ruby={extra.wordRuby} showFurigana={true} fontSize="md" color="orange.400" />
+                  <Text fontSize="xs" color="orange.400">{extra.meaning}</Text>
+                </HStack>
+              ))}
+            </Box>
+          )}
+          {word.notes && <Text fontSize="sm" color="gray.500">{word.notes}</Text>}
+          <Divider mt={1} bg="gray.500" thickness={0.5}/>
+        </Box>
+      ))}
     </Box>
   );
 };
--- src/pages/IT/IT_Words/index.tsx
+++ src/pages/IT/IT_Words/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ITWords from "./IT_Words";
+import { IT_words } from "../../../consts";
 
 const ITWordsPage: React.FC = () => {
   return (
-    <ITWords />
+    <ITWords words={IT_words}/>
   );
 };
 
