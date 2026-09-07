9e02026c05b4d226763163597d96638ed40e2a81
Felipe Zeba <felipe_zeba@outlook.com>
2026-09-08 07:49:57 +0900

    feat: add more ITWords

--- src/consts/IT/IT_words.ts
+++ src/consts/IT/IT_words.ts
@@ -16,5 +16,133 @@ export const IT_words = [
     extraVocabulary: [
       { wordRuby: "実{じっ}数{すう}", meaning: "Real number" },
     ]
-  }
+  },
+  {
+    id: 3,
+    wordRuby: "文{も}字{じ}列{れつ}型{がた}",
+    meaning: "string type",
+    notes: '',
+    extraVocabulary: [
+      { wordRuby: "文{も}字{じ}", meaning: "character" },
+      { wordRuby: "列{れつ}", meaning: "sequence・series" },
+    ]
+  },
+  {
+    id: 4,
+    wordRuby: "文{も}字{じ}型{がた}",
+    meaning: "char type",
+    notes: '',
+  },
+  {
+    id: 5,
+    wordRuby: "論{ろん}理{り}型{がた}",
+    meaning: "boolean type・logical type",
+    notes: '',
+    extraVocabulary: [
+      { wordRuby: "論{ろん}理{り}", meaning: "Logic" },
+    ]
+  },
+  {
+    id: 6,
+    wordRuby: "配{はい}列{れつ}型{がた}",
+    meaning: "array type",
+    notes: '',
+    extraVocabulary: [
+      { wordRuby: "配{はい}列{れつ}", meaning: "Array・arrangement in a sequence" },
+    ]
+  },
+  {
+    id: 7,
+    wordRuby: "構{こう}造{ぞう}体{たい}型{がた}",
+    meaning: "struct type",
+    notes: '',
+    extraVocabulary: [
+      { wordRuby: "構{こう}造{ぞう}", meaning: "structure" },
+      { wordRuby: "体{たい}", meaning: "body・entity" },
+      { wordRuby: "構{こう}造{ぞう}体{たい}", meaning: "struct・structure" },
+      { wordRuby: "構{こう}造{ぞう}体{たい}のメンバーに値{あたい}を代{だい}入{にゅう}する。", meaning: "To assign a value to a struct member" },
+    ]
+  },
+  {
+    id: 8,
+    wordRuby: "要{よう}素{そ}番{ばん}号{ごう}",
+    meaning: "element number・element index (0-1 indexed)",
+    notes: '',
+    extraVocabulary: [
+      { wordRuby: "要{よう}素{そ}", meaning: "Element" },
+      { wordRuby: "要{よう}素{そ}番{ばん}号{ごう}2の値{あたい}", meaning: "The value of element number 2" },
+      { wordRuby: "配{はい}列{れつ}の要{よう}素{そ}番{ばん}号{ごう}を指{し}定{てい}する", meaning: "To specify the element number of an array" },
+      { wordRuby: "要{よう}素{そ}番{ばん}号{ごう}1の値{あたい}を取{しゅ}得{とく}する", meaning: "To get the value of element number 1" },
+      { wordRuby: "添{そえ}字{じ}", meaning: "Index" },
+    ]
+  },
+  {
+    id: 9,
+    wordRuby: "変{へん}数{すう}",
+    meaning: "Variable",
+    notes: '',
+    extraVocabulary: [
+      { wordRuby: "変{へん}", meaning: "Change" },
+      { wordRuby: "数{すう}", meaning: "Number" },
+      { wordRuby: "変{へん}数{すう}の値{あたい}", meaning: "Variable value" },
+    ]
+  },
+  {
+    id: 10,
+    wordRuby: "定{てい}数{すう}",
+    meaning: "Constant",
+    notes: '',
+    extraVocabulary: [
+      { wordRuby: "定{てい}", meaning: "Fixed・constant" },
+      { wordRuby: "数{すう}", meaning: "Number" },
+    ]
+  },
+  {
+    id: 11,
+    wordRuby: "宣{せん}言{げん}する",
+    meaning: "To declare",
+    notes: '',
+    extraVocabulary: [
+      { wordRuby: "変{へん}数{すう}を宣{せん}言{げん}する", meaning: "To declare a variable" },
+    ]
+  },
+  {
+    id: 12,
+    wordRuby: "初{しょ}期{き}化{か}する",
+    meaning: "To initialize",
+    notes: '',
+    extraVocabulary: [
+      { wordRuby: "初{しょ}期{き}", meaning: "initial state・beginning" },
+      { wordRuby: "化{か}", meaning: "change" },
+      { wordRuby: "変{へん}数{すう}を初{しょ}期{き}化{か}する", meaning: "To initialize a variable" },
+      { wordRuby: "初{しょ}期{き}化{か}処{しょ}理{り}", meaning: "Initialization process" },
+    ]
+  },
+  {
+    id: 13,
+    wordRuby: "代{だい}入{にゅう}する",
+    meaning: "To assign",
+    notes: '',
+    extraVocabulary: [
+      { wordRuby: "変{へん}数{すう}に値{あたい}を代{だい}入{にゅう}する", meaning: "To assign a value to a variable" },
+    ]
+  },
+  {
+    id: 14,
+    wordRuby: "A かつ B",
+    meaning: "A && B",
+    notes: '',
+    extraVocabulary: [
+      { wordRuby: "かつ", meaning: "and" },
+    ]
+  },
+  {
+    id: 15,
+    wordRuby: "A または B",
+    meaning: "A || B",
+    notes: '',
+    extraVocabulary: [
+      { wordRuby: "または", meaning: "or" },
+    ]
+  },
 ];
